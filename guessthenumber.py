50e952603dc63efdbca1c2c7ad41956ffd8c88b9
PoojaHrushikesha <poojah.hrushikesha@gmail.com>
2025-07-02 22:05:36 +0530

    Update guessthenumber.py

--- guessthenumber.py
+++ guessthenumber.py
@@ -12,3 +12,23 @@ while guess != secret_number:
         print("Too high!")
     else:
         print("You got it!")
+"""
+        import random
+
+# Generate a random integer between 1 and 10 (inclusive)
+random_number = random.randint(1, 10)
+print(random_number)
+
+# Generate a random floating-point number between 0 and 1
+random_float = random.random()
+print(random_float)
+
+# Choose a random element from a list
+my_list = ['apple', 'banana', 'cherry']
+random_element = random.choice(my_list)
+print(random_element)
+
+# Shuffle a list in place
+random.shuffle(my_list)
+print(my_list)
+"""
