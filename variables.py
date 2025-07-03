02762b73e24a15a3a272ea90f1a0649959d1927e
PoojaHrushikesha <poojah.hrushikesha@gmail.com>
2025-07-03 14:00:42 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,16 @@
+# Declare variables of different types
+
+integer_variable = 10
+float_variable = 3.14
+string_variable = "Hello, Python!"
+list_variable = [1, 2, 3]
+dictionary_variable = {"key": "value"}
+boolean_variable = True
+
+# Print the type of each variable using type()
+print(f"Type of integer_variable: {type(integer_variable)}")
+print(f"Type of float_variable: {type(float_variable)}")
+print(f"Type of string_variable: {type(float_variable)}")
+print(f"Type of list_variable: {type(list_variable)}")
+print(f"Type of dictionary_variable: {type(dictionary_variable)}")
+print(f"Type of boolean_variable: {type(boolean_variable)}")
